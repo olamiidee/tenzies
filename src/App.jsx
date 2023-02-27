b3a23a8ee465323ae042a240419f87f2fbbb2845
olamiidee <osibeluwoolamide@gmail.com>
2023-02-27 23:01:16 +0100

    wooosh

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Die from "./Die";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dice, setDice] = useState(allNewDice());
 
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
+      // console.log(newDice);
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice);
+  }
+
+  const diceElement = dice.map((die) => <Die value={die.value} />);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="flex flex-col justify-center items-center h-[90vh]">
+      <main className="w-[100%] md:max-w-[700px] h-[400px] bg-[#F5F5F5] rounded-[5px] flex flex-col justify-evenly items-center">
+        <div className="px-4 space-y-3">
+          <h1 className="font-semibold text-[2rem]">Tenzies</h1>
+          <p>
+            Roll until all dice are the same. Click each die to freeze it at
+            it's current value between rolls.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-5 gap-4">{diceElement}</div>
+        <div>
+          <button
+            onClick={rollDice}
+            className="h-[50px] w-[100px] text-white text-[1.2rem] font-semibold bg-[#5035FF] rounded-[6px] border-none focus:outline-none roll-dice"
+          >
+            Roll
+          </button>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
