8be074a3d5311af5efd3e6bf2e3a262be0399975
olamiidee <osibeluwoolamide@gmail.com>
2023-02-28 02:31:39 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,71 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+  const { width, height } = useWindowSize();
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      console.log("You Won! ");
+    }
+  }, [dice]);
+
+  function generatenewDie() {
+    return { value: Math.ceil(Math.random() * 6), isHeld: false, id: nanoid() };
+  }
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
+      newDice.push(generatenewDie());
       // console.log(newDice);
     }
     return newDice;
   }
 
   function rollDice() {
-    setDice(allNewDice);
+    if (!tenzies) {
+      setDice((oldDice) =>
+        oldDice.map((die) => {
+          return die.isHeld ? die : generatenewDie();
+        })
+      );
+    } else {
+      setTenzies(false);
+      setDice(allNewDice);
+    }
   }
 
-  const diceElement = dice.map((die) => <Die value={die.value} />);
+  function holdDice(id) {
+    console.log(id);
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElement = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      id={die.id}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
   return (
     <div className="flex flex-col justify-center items-center h-[90vh]">
-      <main className="w-[100%] md:max-w-[700px] h-[400px] bg-[#F5F5F5] rounded-[5px] flex flex-col justify-evenly items-center">
+      <main className="w-[90%] md:max-w-[700px] h-[400px] bg-[#F5F5F5] rounded-[5px] flex flex-col justify-evenly items-center">
         <div className="px-4 space-y-3">
           <h1 className="font-semibold text-[2rem]">Tenzies</h1>
           <p>
@@ -35,12 +78,13 @@ function App() {
         <div>
           <button
             onClick={rollDice}
-            className="h-[50px] w-[100px] text-white text-[1.2rem] font-semibold bg-[#5035FF] rounded-[6px] border-none focus:outline-none roll-dice"
+            className="py-[10px] px-[24px] text-white text-[1.2rem] font-semibold bg-[#5035FF] rounded-[6px] border-none focus:outline-none roll-dice"
           >
-            Roll
+            {tenzies ? "New Game" : "Roll"}
           </button>
         </div>
       </main>
+      {tenzies && <Confetti />}
     </div>
   );
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
   return (
-    <div className="h-[45px] w-[45px] md:w-[50px] md:h-[50px] rounded-[10px] flex justify-center items-center cursor-pointer bg-white die-face ">
+    <div
+      onClick={props.holdDice}
+      className="h-[45px] w-[45px] md:w-[50px] md:h-[50px] rounded-[10px] flex justify-center items-center cursor-pointer die-face"
+      style={styles}
+    >
       <h2 className="text-[1.5rem]">{props.value}</h2>
     </div>
   );
